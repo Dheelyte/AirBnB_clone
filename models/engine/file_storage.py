884cce141cf25925875baae14c66c0ddcf337791
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-02-23 12:10:39 -0800

    Task 5 - Fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,16 +4,29 @@ import json
 from models.base_model import BaseModel
 
 class FileStorage:
+    """
+    Class FileStorage that serializes and deserializes
+    instances to JSON
+        __file_path: the path of the JSON file
+        __objects: a dictionary of all objects
+    """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """Returns a dictionary of all objects"""
         return self.__objects
 
     def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
         self.__objects[f"{self.__class__.__name__}.{obj.id}"] = obj
 
     def save(self):
+        """serializes __objects to the JSON file (path: __file_path)
+            dicttionary: an empty dictionnary
+            Open the dictionary in write mode
+            dump the dictionary in the file f
+        """
         dictionary = {}
         with open(self.__file_path, 'w') as f:
             for obj in self.__objects.values():
@@ -22,6 +35,12 @@ class FileStorage:
             json.dump(dictionary, f)
 
     def reload(self):
+        """deserializes the JSON file to __objects
+            (only if the JSON file (__file_path) exists
+            otherwise, do nothing.
+            If the file doesn’t exist, no exception should be raised)
+            Open in read mode"
+            load the file f and read it"""
         try:
             with open(self.__file_path, 'r') as f:
                 my_dict = json.load(f)
