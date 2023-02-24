872cd39e584df6b5c5a22da346eecd877e3030a7
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-02-24 04:11:20 -0800

    Task 6

--- console.py
+++ console.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 """ contains the entry point of the command interpreter"""
 import cmd
+#from models.base_model import BaseModel
 
+#models = ["BaseModel"]
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
+
+
+    """def do_create(self, model):
+        if model:
+            new = eval("{}()".format(model)
+        elif model not in models:
+            print("** class doesn't exist **")
+        else:
+            print("** class name missing **")"""
 
     def do_quit(self, line):
         return True
@@ -19,7 +30,7 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def emptyline(self):
-        return cmd.Cmd.emptyline(self)
+        pass
 
 
 if __name__ == "__main__":
