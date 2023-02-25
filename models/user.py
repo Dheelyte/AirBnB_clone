d24419db5dfcb2a29237adceb7489c04501ef3ca
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-02-25 13:07:05 -0800

    Update user class

--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """User class that inherits from BaseModel"""
-    email = "s"
+    email = ""
     password = ""
     first_name = ""
     last_name = ""
